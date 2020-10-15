9fde7d51461c1eba7ab0bd748a081203535ee49b
rinalfauzi <rinalfauzi@gmail.com>
2020-10-15 08:52:17 +0700

    Create subclass file

---
+++ src/com/tutorial/HeroStrength.java
@@ -0,0 +1,14 @@
+package com.tutorial;
+
+public class HeroStrength extends Hero {
+
+    //constructor
+    HeroStrength(String name, double defencePower){
+        super(name, defencePower);
+    }
+
+    //overloading constructor(constructor nya sama dengan yang diatas)
+    HeroStrength(String name){
+        super(name);
+    }
+}
--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -1,28 +1,28 @@
 package com.tutorial;
 
-//subclass
-class HeroStreght extends Hero {
+// //subclass
+// class HeroStreght extends Hero {
 
-    //constructor
-    HeroStreght(String name, double defencePower){
-        super(name, defencePower);
-    }
+//     //constructor
+//     HeroStreght(String name, double defencePower){
+//         super(name, defencePower);
+//     }
 
-    //overloading constructor(constructor nya sama dengan yang diatas)
-    HeroStreght(String name){
-        super(name);
-    }
-}
+//     //overloading constructor(constructor nya sama dengan yang diatas)
+//     HeroStreght(String name){
+//         super(name);
+//     }
+// }
 
 public class Main {
     public static void main(String[] args) {
         Hero hero1 = new Hero("Saitama", 10);
         hero1.display();
 
-        HeroStreght hero2 = new HeroStreght("Ucup", 10);
+        HeroStrength hero2 = new HeroStrength("Ucup", 10);
         hero2.display();
 
-        HeroStreght hero3 = new HeroStreght("Otong");
+        HeroStrength hero3 = new HeroStrength("Otong");
         hero3.display();
     }
 }
